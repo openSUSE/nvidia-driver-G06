a845214aa8a741bdc20958a4da5ef6876687ba8f
Stefan Dirsch <sndirsch@suse.de>
2023-10-31 13:23:49 +0100

    %postun: regenerate modules.dep, etc. to avoid dracut failures later (boo#1213765)

--- kmp-postun.sh
+++ kmp-postun.sh
@@ -7,6 +7,12 @@ if [ "$1" = 0 ] ; then
 	# get rid of *all* nvidia kernel modules when uninstalling package (boo#1180010)
 	for dir in $(find /lib/modules  -mindepth 1 -maxdepth 1 -type d); do
                 test -d $dir/updates && rm -f  $dir/updates/nvidia*.ko
+                # generate modules.dep, etc. to avoid dracut failures
+                # later (boo#1213765)
+                if [ -d $dir/kernel ]; then
+                        kversion=$(basename $dir)
+                        depmod $kversion
+                fi
 	done
     fi
     # cleanup of bnc# 1000625
